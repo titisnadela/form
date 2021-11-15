f380d83ac137f32e48263df49326fe0ec5c9e56d
Titis Nadela <titis@pmr.net>
2021-11-15 14:18:01 +0700

    upload

--- lib/form.dart
+++ lib/form.dart
@@ -3,7 +3,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:multiform/user.dart';
+//import 'package:multiform/user.dart';
 import 'package:http/http.dart' as http;
 
 typedef OnDelete();
